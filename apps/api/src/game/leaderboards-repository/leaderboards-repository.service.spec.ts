5a2d09972023ab69ad83e95a2d6ca318e221cd5e
cyberforge12 <50057691+cyberforge12@users.noreply.github.com>
2021-09-04 12:38:07 +0300

    test leaderboards-repository.service.spec.ts

--- apps/api/src/game/leaderboards-repository/leaderboards-repository.service.spec.ts
+++ apps/api/src/game/leaderboards-repository/leaderboards-repository.service.spec.ts
@@ -1,16 +1,24 @@
-import { TestBed } from '@angular/core/testing';
+import { Test, TestingModule } from '@nestjs/testing';
+import { LeaderboardsRepositoryService } from "./leaderboards-repository.service";
+import { Observable } from "rxjs";
+import { LeaderboardsDto } from "../dto/leaderboards.dto";
 
-import { LeaderboardsRepositoryService } from './leaderboards-repository.service';
-
-describe('LeaderboardsRepositoryService', () => {
+describe('MyservService', () => {
   let service: LeaderboardsRepositoryService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LeaderboardsRepositoryService);
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [LeaderboardsRepositoryService],
+    }).compile();
+
+    service = module.get<LeaderboardsRepositoryService>(LeaderboardsRepositoryService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should be defined', () => {
+    expect(service).toBeDefined();
   });
+
+  it('should return Observable', () => {
+    expect(typeof service.getTop()).toBe(typeof new Observable());
+  })
 });
