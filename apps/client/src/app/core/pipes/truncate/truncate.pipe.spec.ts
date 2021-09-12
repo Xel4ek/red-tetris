69be1aa28ce305436dba55024c32ba06f3ceaba3
Xel4ek <l-baal@yandex.ru>
2021-09-12 20:12:13 +0300

    TruncatePipe test

--- apps/client/src/app/core/pipes/truncate/truncate.pipe.spec.ts
+++ apps/client/src/app/core/pipes/truncate/truncate.pipe.spec.ts
@@ -1,8 +1,12 @@
 import { TruncatePipe } from './truncate.pipe';
 
 describe('TruncatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new TruncatePipe();
-    expect(pipe).toBeTruthy();
+  const pipe = new TruncatePipe();
+  it('truncate "less then max"', () => {
+    expect(pipe.transform('wow', 4)).toEqual('wow');
+  });
+
+  it('truncate "more then max"', () => {
+    expect(pipe.transform('so long', 2)).toEqual('so...');
   });
 });
