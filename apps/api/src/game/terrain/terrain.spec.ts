b47263cbd7ff46e216b84fbcd749368a7428835c
cyberforge12 <50057691+cyberforge12@users.noreply.github.com>
2021-09-19 19:32:51 +0300

    api - added terrain tests

--- apps/api/src/game/terrain/terrain.spec.ts
+++ apps/api/src/game/terrain/terrain.spec.ts
@@ -1,7 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { Terrain } from './terrain';
 import { EventEmitter2 } from '@nestjs/event-emitter';
-import { PieceGenerator } from '../../terrain/piece';
+import { Piece, PieceGenerator } from '../../terrain/piece';
+import { last } from "rxjs";
 
 describe('Terrain', () => {
   let terrain: Terrain;
@@ -16,6 +17,41 @@ describe('Terrain', () => {
     pieceGenerator = module.get<PieceGenerator>(PieceGenerator);
     terrain = new Terrain(eventEmitter2, pieceGenerator);
   });
+
+  it('should drop', () => {
+    terrain.drop();
+    expect(terrain.terrain2D.slice(-1)[0].some(value => value !== Terrain.empty)).toBeTruthy();
+  });
+
+  it('should move left', () => {
+    terrain.start();
+    const posX = terrain.x;
+    terrain.move('l');
+    expect(terrain.x === posX - 1).toBeTruthy();
+  });
+
+  it('should move right', () => {
+    terrain.start();
+    const posX = terrain.x;
+    terrain.move('r');
+    expect(terrain.x === posX + 1).toBeTruthy();
+  });
+
+  it('should move down', () => {
+    const posY = terrain.y;
+    terrain.start();
+    terrain.move('d');
+    expect(terrain.y === posY + 1).toBeTruthy();
+  });
+
+  it('should rotate', () => {
+    const piece = terrain.piece.show();
+    terrain.start();
+    terrain.rotate();
+    if (!terrain.piece.show().every((value, idx) => value === Piece.pieceList.S[idx]))
+      expect(terrain.piece.show().some((value, idx) => value != piece[idx])).toBeTruthy();
+  });
+
 
   it('updateScore should be undefined', () => {
     expect(terrain.updateScore(1)).toBeUndefined();
