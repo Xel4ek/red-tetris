ab3a1d66c7e673721f47593b14689089149d2f3a
Xel4ek <l-baal@yandex.ru>
2021-09-19 19:22:48 +0300

    key pipe

--- apps/client/src/app/core/pipes/keys/keys.pipe.ts
+++ apps/client/src/app/core/pipes/keys/keys.pipe.ts
@@ -1,36 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { KeyValue } from '@angular/common';
 
 @Pipe({
   name: 'keys',
 })
 export class KeysPipe implements PipeTransform {
-  transform<K, V>(
-    input: ReadonlyMap<K, V>,
-    compareFn?: (a: KeyValue<K, V>, b: KeyValue<K, V>) => number
-  ): string[];
-  transform<K extends number, V>(
-    input: Record<K, V>,
-    compareFn?: (a: KeyValue<string, V>, b: KeyValue<string, V>) => number
-  ): string[];
+  transform<K, V>(input: ReadonlyMap<K, V>): string[];
+  transform<K extends number, V>(input: Record<K, V>): string[];
   transform<K extends string, V>(
-    input: Record<K, V> | ReadonlyMap<K, V>,
-    compareFn?: (a: KeyValue<K, V>, b: KeyValue<K, V>) => number
+    input: Record<K, V> | ReadonlyMap<K, V>
   ): string[];
-  transform(
-    input: null | undefined,
-    compareFn?: (
-      a: KeyValue<unknown, unknown>,
-      b: KeyValue<unknown, unknown>
-    ) => number
-  ): null;
-  transform<K, V>(
-    input: ReadonlyMap<K, V> | null | undefined,
-    compareFn?: (a: KeyValue<K, V>, b: KeyValue<K, V>) => number
-  ): string[] | null;
+  transform(input: null | undefined): null;
+  transform<K, V>(input: ReadonlyMap<K, V> | null | undefined): string[] | null;
   transform<K extends number, V>(
-    input: Record<K, V> | null | undefined,
-    compareFn?: (a: KeyValue<string, V>, b: KeyValue<string, V>) => number
+    input: Record<K, V> | null | undefined
   ): string[] | null;
   transform<K extends string, V>(
     input: Record<K, V> | ReadonlyMap<K, V> | null | undefined
